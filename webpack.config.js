66e0322090c36b9eb8fdd5e5c55c9b2dcc583cf1
Miro Mannino <miro.mannino@gmail.com>
2020-02-28 20:18:09 +0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "src/js/index.js"),
+  output: {
+    path: path.join(__dirname, "dist/client"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', // Creates `style` nodes from JS strings
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ],
+      },
+    ],
+  },
+  devtool: "source-map"
+};
